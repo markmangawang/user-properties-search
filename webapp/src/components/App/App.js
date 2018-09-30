48bf517998babbde2e1b944ca6560c9a7e403325
Jethro Ordaneza <jet.oneza@gmail.com>
2018-09-30 12:02:30 +0800

    Create directory for components

---
+++ webapp/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Segment, Container } from 'semantic-ui-react';
+
+export default function App() {
+  return (
+    <Segment vertical style={{ padding: '8em 0' }}>
+      <Container>
+        <h1>App</h1>
+      </Container>
+    </Segment>
+  );
+}
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,9 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import App from './components/App';
 
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
